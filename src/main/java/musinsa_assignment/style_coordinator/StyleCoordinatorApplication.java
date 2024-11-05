3f34c1f7d645938912923d8b05ae6798c5ec740d
soob-forest <dltntnqo@naver.com>
2024-11-05 21:06:13 +0900

    chore: init

---
+++ src/main/java/musinsa_assignment/style_coordinator/StyleCoordinatorApplication.java
@@ -0,0 +1,13 @@
+package musinsa_assignment.style_coordinator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StyleCoordinatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StyleCoordinatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/musinsa_assignment/style_coordinator/StyleCoordinatorApplicationTests.java
@@ -0,0 +1,13 @@
+package musinsa_assignment.style_coordinator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StyleCoordinatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
